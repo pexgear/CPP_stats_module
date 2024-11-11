c4ef84f97f47c15cfe8541c340c7e3f0928d6a14
pexgear <44037310+pexgear@users.noreply.github.com>
2024-11-11 17:32:42 +0000

    added delegates for on_changed, on_max and on_min

--- stats.h
+++ stats.h
@@ -3,6 +3,9 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <functional>
+
+using std::string;
 
 template <typename T>
 class stats
@@ -10,26 +13,33 @@ class stats
     public:
         stats() {}
 
-        stats(T _max_stat)
+        stats(string _name, T _max_stat)
         {
-            init(_max_stat);
+            init(_name, _max_stat);
         }
 
-        stats(T _min_stat, T _max_stat)
+        stats(string _name, T _min_stat, T _max_stat)
         {
-            init_range(_min_stat, _max_stat);
+            init(_name, _min_stat, _max_stat);
         }
 
-        ~stats() {}
+        ~stats() 
+        {
+            name.clear();
+            max_stat = 0;
+            min_stat = 0;
+            stat = 0;
+        }
 
     public:
-        virtual void init(T _max_stat)
+        virtual void init(string _name, T _max_stat)
         {
-            init_range(0.0, _max_stat);
+            init(_name, 0, _max_stat);
         }
 
-        virtual void init_range(T _min_stat, T _max_stat)
+        virtual void init(string _name, T _min_stat, T _max_stat)
         {
+            name = _name;
             min_stat = _min_stat;
             max_stat = _max_stat;
             set_stat_unckecked(max_stat);
@@ -76,7 +86,14 @@ class stats
 
         void set_stat_internal(T _stat)
         {
+            int old_stat = stat;
             stat = _stat;
+
+            if (old_stat != _stat)
+            {
+                on_stat_changed(old_stat, stat);
+            }
+            
             if (stat <= min_stat)
             {
                 on_min_stat_reached();
@@ -87,18 +104,56 @@ class stats
             }
         }
 
-        virtual bool is_max_stat_valid()
+        virtual bool is_max_stat_valid() const
         {
             return max_stat > min_stat;
+        }
+
+        virtual bool is_valid() const
+        {
+            return is_max_stat_valid() && name.empty();
         }		
 
-        virtual void on_min_stat_reached() {}
-        virtual void on_max_stat_reached() {}
+        void set_on_changed_delegate(std::function<void(string, T, T)> _delegate)
+        {
+            on_changed_delegate = _delegate;
+        }
+
+        void set_on_max_delegate(std::function<void(string)> _delegate)
+        {
+            on_max_delegate = _delegate;
+        }
+
+        void set_on_min_delegate(std::function<void(string)> _delegate)
+        {
+            on_min_delegate = _delegate;
+        }
+
+        virtual void on_stat_changed(int old_value, int new_value) 
+        {
+            on_changed_delegate(name, old_value, new_value);
+        }
+
+        virtual void on_max_stat_reached() 
+        {
+            on_max_delegate(name);
+        }
+
+        virtual void on_min_stat_reached() 
+        {
+            on_min_delegate(name);
+        }
 
     private:
+        string name;
         T min_stat = 0.0;
         T max_stat = 0.0;
         T stat = 0.0;    
+
+    private:
+        std::function<void(string, T, T)> on_changed_delegate;
+        std::function<void(string)> on_max_delegate;
+        std::function<void(string)> on_min_delegate;
 };
 
 template <typename T>
@@ -107,17 +162,19 @@ class stats_set
     public:
         stats_set() {};
 
-        ~stats_set() {};
+        ~stats_set() {
+            stats_map.clear();
+        };
 
-        virtual void init_stats(const std::vector<std::string> names) 
+        virtual void init_stats(const std::vector<string> _names) 
         {
-            for(const std::string& name : names)
+            for(const string& name : _names)
             {
                 add_stat(name);
             }            
         };
 
-        void add_stat(const std::string& name) 
+        void add_stat(const string& name) 
         {
             if(stats_map.count(name) <= 0)
             {
@@ -125,7 +182,7 @@ class stats_set
             }
         };
 
-        void add_stat(const std::string& name, int max) 
+        void add_stat(const string& name, T max) 
         {
             if(stats_map.count(name) <= 0)
             {
@@ -133,28 +190,84 @@ class stats_set
             }
         };
 
-        void add_stat(const std::string& name, int min, int max) 
+        void add_stat(const string& _name, T _min, T _max) 
         {
-            if(stats_map.count(name) <= 0)
+            if(stats_map.count(_name) <= 0)
             {
-                add_or_replace_stat(name, min, max);
+                add_or_replace_stat(_name, _min, _max);
             }
         };
 
-        void add_or_replace_stat(const std::string& name) 
+        void add_or_replace_stat(const string& _name) 
         {
-            stats_map[name] = stats<T>(); 
+            add_or_replace_stat(_name, 0.0, 0.0); 
         };
 
-        void add_or_replace_stat(const std::string& name, int max) 
+        void add_or_replace_stat(const string& _name, T _max) 
         {
-            stats_map[name] = stats<T>(max); 
+            add_or_replace_stat(_name, 0.0, _max);
         };
 
-        void add_or_replace_stat(const std::string& name, int min, int max) 
+        void add_or_replace_stat(const string& _name, T _min, T _max) 
         {
-            stats_map[name] = stats<T>(min, max); 
+            stats_map[_name] = stats<T>(_name, _min, _max);
+
+            set_on_changed_delegate(_name, [this]
+            (std::string _name, T _old_value, T _new_value) {
+                this->on_changed(_name, _old_value, _new_value);
+            });
+            set_on_min_delegate(_name, [this]
+            (std::string _name) {
+                this->on_min(_name);
+            });
+            set_on_max_delegate(_name, [this]
+            (std::string _name) {
+                this->on_max(_name);
+            });
         };
 
-        std::map<std::string, stats<T>> stats_map;
+        stats<T> get_stat_by_name(const string& _name)
+        {
+            return stats_map[_name];
+        }
+
+        void set_on_changed_delegate(std::function<void(string, T, T)> _delegate)
+        {
+            on_changed_delegate = _delegate;
+        }
+
+        void set_on_min_delegate(std::function<void(string, T, T)> _delegate)
+        {
+            on_min_delegate = _delegate;
+        }
+
+        void set_on_max_delegate(std::function<void(string, T, T)> _delegate)
+        {
+            on_max_delegate = _delegate;
+        }
+
+        void set_on_changed_delegate(const string& _name, std::function<void(string, T, T)> _delegate)
+        {
+            get_stat_by_name(_name).set_on_changed_delegate(_delegate);
+        }
+
+        void set_on_max_delegate(const string& _name, std::function<void(string)> _delegate)
+        {
+            get_stat_by_name(_name).set_on_max_delegate(_delegate);
+        }
+
+        void set_on_min_delegate(const string& _name, std::function<void(string)> _delegate)
+        {
+            get_stat_by_name(_name).set_on_min_delegate(_delegate);
+        }
+
+        virtual void on_changed(string _name, T _old_value, T _new_value) {};
+        virtual void on_min(string _name) {};
+        virtual void on_max(string _name) {};
+
+        std::function<void(string, T, T)> on_changed_delegate;
+        std::function<void(string)> on_min_delegate;
+        std::function<void(string)> on_max_delegate;
+
+        std::map<string, stats<T>> stats_map;
 };
